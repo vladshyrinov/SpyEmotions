75c58ebada074d798a5309972a0e3a701c142fdf
Valentyn Kyianytsia <valentine1004@ukr.net>
2018-02-04 07:23:49 +0200

    last commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { WebcamModule } from './modules/webcam/webcam.module';
 import { HttpClientModule } from '@angular/common/http';
@@ -8,18 +8,26 @@ import { EmotionsService } from './services/emotions.service';
 import { AdvisedProfListComponent } from './components/advised-prof-list/advised-prof-list.component';
 import { AdvisedProfComponent } from './components/advised-prof-list/advised-prof/advised-prof.component';
 import { HomeComponent } from './components/home/home.component';
+import { LoaderComponent } from './components/loader/loader.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'advised', component: AdvisedProfListComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     AdvisedProfListComponent,
     AdvisedProfComponent,
-    HomeComponent
+    HomeComponent,
+    LoaderComponent
   ],
   imports: [
     BrowserModule,
     WebcamModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [EmotionsService],
   bootstrap: [AppComponent]
--- src/app/components/advised-prof-list/advised-prof-list.component.ts
+++ src/app/components/advised-prof-list/advised-prof-list.component.ts
@@ -8,34 +8,33 @@ import { Profession } from '../../profession';
 export class AdvisedProfListComponent implements OnInit {
   professions: Profession[] = [
     // tslint:disable-next-line:max-line-length
-    { id: 1, name: "Military", img: "../../../assets/image/military.jpg", description: "Some text some text some text", links: ["link1", "link2", "link3"] },
+    { id: 1, name: "Business", img: "../../../assets/image/business.jpg", description: "Some text some text some text", links: ["link1", "link2", "link3"] },
     // tslint:disable-next-line:max-line-length
-    { id: 2, name: "Doctor", img: "../../../assets/image/doctor.jpg", description: "Some text some text some text", links: ["link1", "link2", "link3"] },
+    { id: 2, name: "Creative", img: "../../../assets/image/creative.jpg", description: "Some text some text some text", links: ["link1", "link2", "link3"] },
     // tslint:disable-next-line:max-line-length
-    { id: 3, name: "Business", img: "../../../assets/image/business.jpg", description: "Some text some text some text", links: ["link1", "link2", "link3"] },
+    { id: 3, name: "Culinary", img: "../../../assets/image/cooker.jpg", description: "Some text some text some text", links: ["link1", "link2", "link3"] },
     // tslint:disable-next-line:max-line-length
-    { id: 4, name: "Teacher", img: "../../../assets/image/teacher.jpg", description: "Some text some text some text", links: ["link1", "link2", "link3"] },
+    { id: 4, name: "Engineering", img: "../../../assets/image/engineer.jpg", description: "Some text some text some text", links: ["link1", "link2", "link3"] },
     // tslint:disable-next-line:max-line-length
-    { id: 5, name: "Service", img: "../../../assets/image/service.jpg", description: "Some text some text some text", links: ["link1", "link2", "link3"] },
+    { id: 5, name: "Fashion", img: "../../../assets/image/fashion.jpg", description: "Some text some text some text", links: ["link1", "link2", "link3"] },
     // tslint:disable-next-line:max-line-length
-    { id: 6, name: "Engineer", img: "../../../assets/image/engineer.jpg", description: "Some text some text some text", links: ["link1", "link2", "link3"] },
+    { id: 6, name: "IT", img: "../../../assets/image/it.jpg", description: "Some text some text some text", links: ["link1", "link2", "link3"] },
     // tslint:disable-next-line:max-line-length
-    { id: 7, name: "Cooker", img: "../../../assets/image/cooker.jpg", description: "Some text some text some text", links: ["link1", "link2", "link3"] },
+    { id: 7, name: "Media", img: "../../../assets/image/media.jpg", description: "Some text some text some text", links: ["link1", "link2", "link3"] },
     // tslint:disable-next-line:max-line-length
-    { id: 8, name: "IT", img: "../../../assets/image/it.jpg", description: "Some text some text some text", links: ["link1", "link2", "link3"] },
+    { id: 8, name: "Medicine", img: "../../../assets/image/doctor.jpg", description: "Some text some text some text", links: ["link1", "link2", "link3"] },
     // tslint:disable-next-line:max-line-length
-    { id: 9, name: "Creative", img: "../../../assets/image/creative.jpg", description: "Some text some text some text", links: ["link1", "link2", "link3"] },
+    { id: 9, name: "Military", img: "../../../assets/image/military.jpg", description: "Some text some text some text", links: ["link1", "link2", "link3"] },
     // tslint:disable-next-line:max-line-length
-    { id: 10, name: "Fashion", img: "../../../assets/image/fashion.jpg", description: "Some text some text some text", links: ["link1", "link2", "link3"] },
+    { id: 10, name: "Pedagogy", img: "../../../assets/image/teacher.jpg", description: "Some text some text some text", links: ["link1", "link2", "link3"] },
+     // tslint:disable-next-line:max-line-length
+     { id: 11, name: "Politic", img: "../../../assets/image/politic.jpg", description: "Some text some text some text", links: ["link1", "link2", "link3"] },
     // tslint:disable-next-line:max-line-length
-    { id: 11, name: "Media", img: "../../../assets/image/media.jpg", description: "Some text some text some text", links: ["link1", "link2", "link3"] },
+    { id: 12, name: "Service", img: "../../../assets/image/service.jpg", description: "Some text some text some text", links: ["link1", "link2", "link3"] },
     // tslint:disable-next-line:max-line-length
-    { id: 12, name: "Politic", img: "../../../assets/image/politic.jpg", description: "Some text some text some text", links: ["link1", "link2", "link3"] },
+    { id: 13, name: "Sport", img: "../../../assets/image/sport.jpg", description: "Some text some text some text", links: ["link1", "link2", "link3"] },
     // tslint:disable-next-line:max-line-length
-    { id: 13, name: "Transport", img: "../../../assets/image/transport.jpg", description: "Some text some text some text", links: ["link1", "link2", "link3"] },
-    // tslint:disable-next-line:max-line-length
-    { id: 14, name: "Sport", img: "../../../assets/image/sport.jpg", description: "Some text some text some text", links: ["link1", "link2", "link3"] }
-
+    { id: 14, name: "Transport", img: "../../../assets/image/transport.jpg", description: "Some text some text some text", links: ["link1", "link2", "link3"] }
 
   ];
   constructor() { }
